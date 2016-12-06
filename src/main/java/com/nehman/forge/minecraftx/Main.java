8a37af7562a37900b795c52dea6cb6582233e126
NehManAlmighty <realnehman@gmail.com>
2016-12-06 15:45:12 +0530

    Update Main.java

--- src/main/java/com/nehman/forge/minecraftx/Main.java
+++ src/main/java/com/nehman/forge/minecraftx/Main.java
@@ -10,11 +10,11 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = Main.MODID, name = Main.MODNAME, version = Main.VERSION)
 public class Main {
-	public static final String MODID = "nehmanalmighty";
-    public static final String MODNAME = "NehMan Almighty";
+	public static final String MODID = "minecraftx";
+    public static final String MODNAME = "MinecraftX";
     public static final String VERSION = "1.0.0";
     
-    @SidedProxy(clientSide="com.nehman.forge.nehmanalmighty.ClientProxy", serverSide="com.nehman.forge.nehmanalmighty.ServerProxy")
+    @SidedProxy(clientSide="com.nehman.forge.minecraftx.ClientProxy", serverSide="com.nehman.forge.minecraftx.ServerProxy")
     @Instance
     public static Main instance;
     public static CommonProxy proxy;
